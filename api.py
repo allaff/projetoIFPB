854579c92f637add51123fce4bcc2be84616aa4c
Ginaldo Ribeiro <ginaldo.ribeiro@gmail.com>
2022-05-23 11:11:20 -0300

    add name e password

--- api.py
+++ api.py
@@ -1,13 +1,11 @@
 import os
 import requests
 from flask import *
-
 app = Flask(__name__)
 
-
-@app.route("/primeirarota/<nome>")
-def ok(nome):
-    return "Nome: "+nome
-
+@app.route("/primeirarota/<nome>/<senha>")
+def ok(nome,senha):
+    return "Nome: "+nome +' '+"Senha: "+senha
 
 app.run(host="0.0.0.0", port=2000, debug=False)
+
