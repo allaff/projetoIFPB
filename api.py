441f10f9f6cececf1e7cbc5774374a8a206bafc4
Ginaldo Ribeiro <ginaldo.ribeiro@gmail.com>
2022-05-23 00:45:20 -0300

    backend funcionando

--- api.py
+++ api.py
@@ -1,3 +1,13 @@
 import os
 import requests
 from flask import *
+
+app = Flask(__name__)
+
+
+@app.route("/primeirarota/<nome>")
+def ok(nome):
+    return "Nome: "+nome
+
+
+app.run(host="0.0.0.0", port=2000, debug=False)
