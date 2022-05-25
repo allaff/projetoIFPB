eec3723d4294f4be26fc9f1a7dd397b3ed6fe18f
Ginaldo Ribeiro <ginaldo.ribeiro@gmail.com>
2022-05-25 18:34:04 -0300

    commit testes

--- api.py
+++ api.py
@@ -1,11 +1,13 @@
 import os
+from webbrowser import get
 import requests
 from flask import *
 app = Flask(__name__)
 
-@app.route("/primeirarota/<nome>/<senha>")
-def ok(nome,senha):
-    return "Nome: "+nome +' '+"Senha: "+senha
-
+@app.route("/primeirarota/", methods=['post', 'get'])
+def ok(nome, senha):
+    if request.form == 'post':
+        return str(request.form)
+    elif request.form == 'get':
+        return 'Nome: '+nome+' '+'Senha: '+senha
 app.run(host="0.0.0.0", port=2000, debug=False)
-
